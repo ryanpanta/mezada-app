ff96dcde27fb197d872abea0680203ae70b8656f
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2025-02-03 18:07:53 -0300

    styles: added more styles and finished dashboard screen

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -3,7 +3,7 @@ import { colors } from "../../styles/color";
 import { fontFamily } from "../../styles/fontFamily";
 import CustomButton from "../../components/Form/CustomButtom";
 import React from "react";
-import { LayoutList } from "lucide-react-native";
+import { LayoutList, Hourglass, CircleCheck, Ban, UsersRound, ChartColumnIncreasing, ChevronRight } from "lucide-react-native";
 import { Link } from "expo-router";
 
 const DashBoard = () => {
@@ -42,8 +42,8 @@ const DashBoard = () => {
                 </Text>
 
                 <View style={styles.taskBackground}>
-                    <View style={styles.totalContainer}>
-                        <View style={styles.totalWrapper}>
+                    <View style={styles.itemContainer}>
+                        <View style={[styles.totalWrapper, styles.wrapper]}>
                             <LayoutList color={"#434343"} />
                         </View>
                         <View>
@@ -51,33 +51,61 @@ const DashBoard = () => {
                             <Text style={styles.label}>Total</Text>
                         </View>
                     </View>
-                    <View style={styles.waitingContainer}>
-                        <View style={styles.totalWrapper}>
-                            <LayoutList color={"#434343"} />
+                    <View style={styles.itemContainer}>
+                        <View style={[styles.waitingWrapper, styles.wrapper]}>
+                            <Hourglass color={"#5F5C0F"} />
                         </View>
                         <View>
                             <Text style={styles.countValue}>25</Text>
-                            <Text style={styles.label}>Total</Text>
+                            <Text style={styles.label}>Pendentes</Text>
                         </View>
                     </View>
-                    <View style={styles.totalContainer}>
-                        <View style={styles.totalWrapper}>
-                            <LayoutList color={"#434343"} />
+                    <View style={styles.itemContainer}>
+                        <View style={[styles.approvedWrapper, styles.wrapper]}>
+                            <CircleCheck color={"#008012"} />
                         </View>
                         <View>
                             <Text style={styles.countValue}>25</Text>
-                            <Text style={styles.label}>Total</Text>
+                            <Text style={styles.label}>Aprovadas</Text>
                         </View>
                     </View>
-                    <View style={styles.totalContainer}>
-                        <View style={styles.totalWrapper}>
-                            <LayoutList color={"#434343"} />
+                    <View style={styles.itemContainer}>
+                        <View style={[styles.rejectedWrapper, styles.wrapper]}>
+                            <Ban color={"#BD0909"} />
                         </View>
                         <View>
                             <Text style={styles.countValue}>25</Text>
-                            <Text style={styles.label}>Total</Text>
+                            <Text style={styles.label}>Rejeitadas</Text>
                         </View>
                     </View>
+                </View>
+            </View>
+
+            <View style={styles.actionsContainer}>
+                <Text style={styles.titleSection}>
+                    Ações
+                </Text>
+
+                <View style={styles.actionBackground}>
+                    <View style={styles.itemContainerAction}>
+                        <View style={[styles.approvedWrapper, styles.wrapper]}>
+                            <UsersRound color={"#008012"} />
+                        </View>
+                        <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+                            <Text style={styles.actionText}>Ver informações do grupo familiar</Text>
+                            <ChevronRight color={"#8B8B8B"} />
+                        </View>
+                    </View>
+                    <View style={styles.itemContainerAction}>
+                        <View style={[styles.approvedWrapper, styles.wrapper]}>
+                            <ChartColumnIncreasing color={"#008012"} />
+                        </View>
+                        <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+                            <Text style={styles.actionText}>Encerrar o ciclo de mesada e consultar sugestão de valor</Text>
+                            <ChevronRight color={"#8B8B8B"} />
+                        </View>
+                    </View>
+                    
                 </View>
             </View>
         </View>
@@ -124,27 +152,63 @@ const styles = StyleSheet.create({
     tasksContainer: {
         marginTop: 40,
     },
+    actionsContainer: {
+        marginTop: 40,
+    },
     taskBackground: {
         marginTop: 10,
         padding: 20,
-        backgroundColor: "rgba(82, 167, 94, 0.2)",
+        backgroundColor: "rgba(82, 167, 94, 0.1)",
         borderRadius: 20,
         flexDirection: "row",
         justifyContent: "space-between",
         gap: 30,
         flexWrap: "wrap",
     },
-    totalContainer: {
+    actionBackground: {
+        marginTop: 10,
+        padding: 20,
+        backgroundColor: "rgba(82, 167, 94, 0.1)",
+        borderRadius: 20,
+        gap: 30,
+    },
+    itemContainer: {
         flexDirection: "row",
         justifyContent: "flex-start",
         gap: 10,
     },
-    totalWrapper: {
-        backgroundColor: "#BABABA",
+    itemContainerAction: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+        flex: 1,
+        maxWidth: "100%",
+    },
+    wrapper: {
         alignItems: "center",
         justifyContent: "center",
         borderRadius: "50%",
         padding: 12,
+    },
+    totalWrapper: {
+        backgroundColor: "#BABABA",
+        boxShadow: "0px 4px 15px 4px rgba(186, 186, 186, 0.6)",
+        
+    },
+    waitingWrapper: {
+        backgroundColor: "#EAE793",
+        boxShadow: "0px 4px 15px 4px rgba(234, 231, 147, 0.6)",
+
+    },
+    approvedWrapper: {
+        backgroundColor: "#7ED68A",
+        boxShadow: "0px 4px 15px 4px rgba(126, 214, 138, 0.6)",
+
+    },
+    rejectedWrapper: {
+        backgroundColor: "#F1A0A0",
+        boxShadow: "0px 4px 15px 4px rgba(241, 160, 160, 0.6)",
+
     },
     countValue: {
         fontFamily: fontFamily.roboto.bold,
@@ -156,6 +220,12 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: colors.gray[800],
     },
+    actionText: {
+        fontFamily: fontFamily.roboto.regular,
+        fontSize: 16,
+        color: colors.black,
+        maxWidth: "90%",
+    }
 });
 
 export default DashBoard;
